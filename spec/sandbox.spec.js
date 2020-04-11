531c2b0100ffb996f5e4a5d6f9159ca2a03de71c
Girma Nigusse <xgirma@users.noreply.github.com>
2020-04-11 18:56:48 -0400

    Initial commit

---
+++ spec/sandbox.spec.js
@@ -0,0 +1,31 @@
+import test from "ava";
+import * as wd from "wd";
+import config from "../config";
+
+const url = "https://e2e-boilerplate.github.io/sandbox/";
+
+let browser;
+
+test.before(async () => {
+  browser = wd.promiseChainRemote();
+  return config(url, browser);
+});
+
+test.after(async () => {
+  return browser.quit();
+});
+
+test("should be on Sandbox", async (t) => {
+  return browser.title().then((title) => {
+    t.is(title, "Sandbox");
+  });
+});
+
+test("should have a page header", async (t) => {
+  return browser
+    .elementByTagName("h1")
+    .text()
+    .then((header) => {
+      t.is(header, "Sandbox");
+    });
+});
